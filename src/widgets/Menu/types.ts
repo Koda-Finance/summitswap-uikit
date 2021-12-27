06a45a72566903a9bf9f324aa5d3a9213c8b8c50
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-12-28 01:52:55 +0500

    make isShowConnect type is optional

--- src/widgets/Menu/types.ts
+++ src/widgets/Menu/types.ts
@@ -36,7 +36,7 @@ export interface MenuEntry {
   href?: string;
   calloutClass?: string;
   initialOpenState?: boolean;
-  isShowConnect: boolean;
+  isShowConnect?: boolean;
 }
 
 export interface PanelProps {
