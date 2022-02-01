448caa3e216af9dee839a510a7dbc493afac7e00
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-02-01 19:55:00 +0400

    feat(icons): add referral icon

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2438,6 +2438,11 @@ var Icon$1j = function (props) {
         React.createElement("path", { d: "M27.0492 7.96817L13.3685 0L0.0283832 7.82485L0 23.5892L13.4536 31.5574L27.0492 23.7612V7.96817ZM13.3969 2.40765L23.5013 8.31212L13.4536 14.2166L3.34922 8.31212L13.3969 2.40765ZM12.2332 28.2899L2.07197 22.414V10.6624L12.2048 16.5669V28.2899H12.2332ZM14.8444 16.7389L24.9772 10.8344V22.5574L14.8444 28.4905V16.7389Z" })));
 };
 
+var Icon$1k = function (props) {
+    return (React.createElement(Svg, __assign({ viewBox: "0 0 24 24" }, props),
+        React.createElement("image", { xlinkHref: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAACbklEQVRIibXVSaiOYRQH8N/9zDNRLG6EmxIJKfMCxYKMGxYsiKIkobiiZFgIKUTGTLEQioWQhSFDLpmKiNQl88ztItfieb68Xu93fRb+9S7ec85z/md8npKamhr/E3VL7uwvxq4LZqAHWuAHXuA0tqOq0MHcXxyX4SbuYhQ+4GqUNcRifMHWghnU4nwZluIIhuB1AbvBOITp6IOKYgh2Ywq6404tQcA5tEW5kN1wnMors0pUHp23zHA+D7dwPkaexCpMw0l0KERQhpXoi/cpXQXW4Cge4SwWpGx2YofQfFDi9r6kwQE0wtjUwU2YgHYJWS9cQ0/cSNm/xUwcTGaQw0ZM9idGC+kncV0YgPIM+72Yk3eax5hI8DHjQA5vMuSf0DRDvgcdkUtn0DzDGCoxMUM+GCcy5BXCMvbIYS7eYR1Khcm5JzEJWILZaJ2QzUErbMggqMFXtK2LS1gkNG2cMClN/D5FJ7ENV9AZ7bE+nimEOqjO4SI2x1Q/YJfQi3fRsBlmYRIeRNln4f5Zi5EZztugPh4ke/AYWxL/g4R5f4XlWI0RUfdaKOd77Ec1DqNx1A+PAVQmCSrwDcPi/zmcEaahNVakonyD8cLGD40BHYu66fHsH5tcKtS6Hp5gIZ5mlCCNC0JPqiLhIHE/0gTzo/ONwl78C7rhudDLtXiWRQD9hcelF17+A0FDTI0kC/PCLIJKdBVKdVkYt2IwULibfsu80It2N0ZUD9+FG3SEMLJ5lArL9yLqyjAg7ai2F60avdFP2PIDkSz//jaK/8eFZf2U5aQ2gjwuJSLrFCOvwn2/lrEQcsUQJPEwfsWiwU/awI84+iwB5AAAAABJRU5ErkJggg==", width: "24", height: "24" })));
+};
+
 var IconModule = /*#__PURE__*/Object.freeze({
     __proto__: null,
     CollectiblesIcon: Icon$V,
@@ -2467,7 +2472,8 @@ var IconModule = /*#__PURE__*/Object.freeze({
     TradeIcon: Icon$1g,
     TrustScoresIcon: Icon$1h,
     TwitterIcon: Icon$1i,
-    RubicIcon: Icon$1j
+    RubicIcon: Icon$1j,
+    ReferralIcon: Icon$1k
 });
 
 var links = [
