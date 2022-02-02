c4ebdb18ad8b318b87fc15b0f508972c308839b2
Luka Samkharadze <lukesamkharadze@gmail.com>
2022-02-03 01:20:53 +0400

    chores: build

--- dist/widgets/Menu/types.d.ts
+++ dist/widgets/Menu/types.d.ts
@@ -30,6 +30,7 @@ export interface MenuEntry {
     href?: string;
     calloutClass?: string;
     initialOpenState?: boolean;
+    showConnectButton?: boolean;
 }
 export interface PanelProps {
     isDark: boolean;
