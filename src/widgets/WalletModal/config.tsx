3f9659719301372a25e5081aecc019b08eb512fe
erwin-huang <erwinhuank@gmail.com>
2022-02-08 11:16:48 +0700

    add deep link for wallets

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -12,36 +12,43 @@ const connectors: Config[] = [
     title: "Metamask",
     icon: Metamask,
     connectorId: ConnectorNames.Injected,
+    redirectUrl: `https://metamask.app.link/dapp/${window.location.href.replace(/^https?:\/\//, "")}`,
   },
   {
     title: "TrustWallet",
     icon: TrustWallet,
     connectorId: ConnectorNames.WalletConnect,
+    redirectUrl: "",
   },
   {
     title: "MathWallet",
     icon: MathWallet,
     connectorId: ConnectorNames.Injected,
+    redirectUrl: `mathwallet://mathwallet.org?action=link&value=${window.location.href}`,
   },
   {
     title: "TokenPocket",
     icon: TokenPocket,
     connectorId: ConnectorNames.Injected,
+    redirectUrl: `https://tokenpocket.github.io/applink?dappUrl=${window.location.href}`,
   },
   {
     title: "WalletConnect",
     icon: WalletConnect,
     connectorId: ConnectorNames.WalletConnect,
+    redirectUrl: "",
   },
   {
     title: "Binance Chain Wallet",
     icon: BinanceChain,
     connectorId: ConnectorNames.BSC,
+    redirectUrl: "",
   },
   {
     title: "SafePal Wallet",
     icon: SafePalWallet,
     connectorId: ConnectorNames.WalletConnect,
+    redirectUrl: "",
   },
 ];
 
