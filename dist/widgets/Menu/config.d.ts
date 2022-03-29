1b51a500ed8a22349eb4495ab3220797521302bc
erwin-huang <erwinhuank@gmail.com>
2022-03-29 13:58:53 +0700

    chore: build

---
+++ dist/widgets/Menu/config.d.ts
@@ -0,0 +1,24 @@
+export declare const links: {
+    label: string;
+    icon: string;
+    href: string;
+}[];
+export declare const socials: ({
+    label: string;
+    icon: string;
+    items: {
+        label: string;
+        href: string;
+    }[];
+    href?: undefined;
+} | {
+    label: string;
+    icon: string;
+    href: string;
+    items?: undefined;
+})[];
+export declare const MENU_HEIGHT_INCREASED = 135;
+export declare const MENU_HEIGHT = 85;
+export declare const MENU_ENTRY_HEIGHT = 43;
+export declare const SIDEBAR_WIDTH_FULL = 240;
+export declare const SIDEBAR_WIDTH_REDUCED = 56;
