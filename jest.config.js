3b7934bbb466fda78a124981bc9dbcec8be80726
erwin-huang <erwinhuank@gmail.com>
2022-07-12 14:39:54 +0700

    fix: test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: "ts-jest",
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/.storybook/"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(lightweight-charts|fancy-canvas)/)"
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
